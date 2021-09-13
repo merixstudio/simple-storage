5ef28de5eac344b56a9df3ed3955c49c510be677
Tomasz Szymański <t.szymanski@merixstudio.com>
2021-09-13 15:45:34 +0200

    full tests coverage of AWS storage

--- storages/backends/amazon_s3.py
+++ storages/backends/amazon_s3.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from os import environ
 from tempfile import SpooledTemporaryFile
 from typing import AnyStr
 
@@ -16,8 +17,13 @@ class AmazonS3Storage(Storage):
     _SERVICE_NAME = "s3"
 
     def __init__(self, aws_access_key_id: str, aws_secret_access_key: str, bucket_name: str):
-        self._session: Session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-        self._s3: ServiceResource = self._session.resource(service_name=self._SERVICE_NAME)
+        self._session: Session = boto3.Session(
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
+        self._s3: ServiceResource = self._session.resource(
+            service_name=self._SERVICE_NAME
+        )
         self._bucket_name = bucket_name
         self._bucket = self._s3.Bucket(self._bucket_name)
 
@@ -26,17 +32,19 @@ class AmazonS3Storage(Storage):
 
     def read(self, name: str, mode: str = "r") -> AnyStr:
         body: StreamingBody = self._bucket.Object(key=name).get()["Body"]
-        return body.read()
+        return body.read() if "b" in mode else body.read().decode("utf-8")
 
     def write(self, name: str, content: AnyStr, mode: str = "a"):
-        self._bucket.put_object(Body=content) # TODO
+        self._bucket.put_object(Key=name, Body=content)
 
     def delete(self, name: str):
         self._bucket.Object(key=name).delete()
 
     def exists(self, name: str) -> bool:
         try:
-            self._session.client(service_name=self._SERVICE_NAME).head_object(Bucket=self._bucket_name, Key=name)
+            self._session.client(
+                service_name=self._SERVICE_NAME
+            ).head_object(Bucket=self._bucket_name, Key=name)
             return True
         except ClientError as cause:
             logging.info(cause, exc_info=True)
@@ -56,7 +64,7 @@ class AmazonS3Storage(Storage):
 
 
 amazon_s3_storage = AmazonS3Storage(
-    aws_access_key_id="",
-    aws_secret_access_key="",
-    bucket_name="",
+    aws_access_key_id=environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=environ.get("AWS_SECRET_ACCESS_KEY"),
+    bucket_name=environ.get("AWS_S3_BUCKET_NAME"),
 )
--- tests/test_aws_s3_storage.py
+++ tests/test_aws_s3_storage.py
@@ -1,18 +1,74 @@
+from datetime import datetime, timedelta, tzinfo, timezone
 from unittest import TestCase
+from uuid import uuid4
 
 import pytest
 
+from storages.backends.base import Storage
 from storages.backends.amazon_s3 import amazon_s3_storage
 
 
+class aws_temp_file:
+    CONTENT = "Lorem ipsum dolor sit amet..."
+    CONTENT_BINARY = b"Binary lorem ipsum dolor sit amet..."
+
+    def __init__(self, storage: Storage, binary: bool = False):
+        self._storage = storage
+        self._binary = binary
+        self._file_name = str(uuid4())
+
+    def __enter__(self):
+        self._storage.write(
+            name=self._file_name,
+            content=self.CONTENT_BINARY if self._binary else self.CONTENT,
+            mode="xb" if self._binary else "x",
+        )
+        return self._file_name
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._storage.delete(self._file_name)
+
+
 class TestAWSS3Storage(TestCase):
+    _DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
     @pytest.fixture(autouse=True)
     def init_storage(self, tmpdir):
         self._storage = amazon_s3_storage
 
-    def test_read(self):
-        data = self._storage.read(name="test-coverage.png", mode="rb")
-        print("xxx")
-        print(data)
-        print("xxx")
-        assert False
+    def test_file_not_exists(self):
+        assert not self._storage.exists("some_non_existent.file")
+
+    def test_file_exists_upon_writing(self):
+        with aws_temp_file(storage=self._storage) as temp_file:
+            assert self._storage.exists(temp_file)
+
+    def test_file_contains_written_data(self):
+        with aws_temp_file(storage=self._storage) as temp_file:
+            assert self._storage.read(temp_file) == aws_temp_file.CONTENT
+
+    def test_file_contains_binary_written_data(self):
+        with aws_temp_file(storage=self._storage, binary=True) as temp_file:
+            assert self._storage.read(temp_file, mode="rb") == aws_temp_file.CONTENT_BINARY
+
+    def test_file_does_not_exist_upon_writing_and_deletion(self):
+        with aws_temp_file(storage=self._storage) as temp_file:
+            self._storage.delete(temp_file)
+            assert not self._storage.exists(temp_file)
+
+    def test_file_size_matches_content_size(self):
+        with aws_temp_file(storage=self._storage) as temp_file:
+            assert self._storage.size(temp_file) == len(aws_temp_file.CONTENT)
+
+    def test_file_creation_time(self):
+        with pytest.raises(Exception):
+            self._storage.get_created_time("any_name.ext")
+
+    def test_file_access_time(self):
+        with pytest.raises(Exception):
+            self._storage.get_access_time("any_name.ext")
+
+    def test_file_modification_time(self):
+        with aws_temp_file(storage=self._storage) as temp_file:
+            modification_time = self._storage.get_modified_time(temp_file).replace(tzinfo=None)
+            assert modification_time - datetime.now() < timedelta(seconds=10)
