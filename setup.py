cc93887a96efc3902ceb8266bd5709759ed47dcf
Tomasz Szymański <t.szymanski@merixstudio.com>
2021-09-17 09:45:55 +0200

    using static setup.cfg for reading README.md as long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as file_handler:
-    long_description = file_handler.read()
-
-setup(
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-)
+setup()
