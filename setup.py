d77c54abbbc2b48848ce72dcd2a334d984534151
Tomasz Szymański <t.szymanski@merixstudio.com>
2021-09-17 09:29:19 +0200

    import long_description from README.md

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
 from setuptools import setup
 
-setup()
+setup(
+    long_description=(Path(__file__) / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+)
